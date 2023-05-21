30d4c61c4a1241742e8667d7d40d8e5a430ce039
itheima <itheima@itheima.cn>
2023-05-21 10:28:17 +0800

    index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -28,5 +28,16 @@ function renderAvatar(user){
     // 获取用户的名称
     var name = user.nickname || user.username
     $('#welcome').html('欢迎&nbsp;&nbsp;' + name)
+    // 按需求渲染用户的头像
+    if(user.user.pic !== null){
+        //3.1渲染图片头像
+        $('.layui-nav-img').attr('src',user.user_pic).show()
+        $('.text-avatar').hide()
+    }else{
+        //渲染文本头像
+        $('.layui-nav-img').hide()
+        var first = name[0].toUpperCase()
+        $('.text-avatar').html(first).show()
+    }
 }
 
